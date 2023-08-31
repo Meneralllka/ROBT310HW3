cabb35fd58f15ca28c9a838638de8c49bb58df06
filya <filya301087@gmail.com>
2023-08-31 19:27:09 +0600

    Added function to create an image based on the parallelogram kernel

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 '''
 1 (65%) - main task accomplishment -- DONE
 2 (5%) - for development of personal image database for creating the mosaic -- DONE
-3 (5%) - for implementation of a different shape (circular, parallelogram, etc…)
+3 (5%) - for implementation of a different shape (circular, parallelogram, etc…) -- DONE
 4 (5%) - awarded if the size of a patch is adjustable -- DONE
 5 (10%) For questions 1‐4, create a document consisting of your inputs, outputs, your observations,
 problems you have faced, solutions indicating how you have overcome the problems, and other points
 that you think are necessary.
-6 (10%) For questions 1‐5, create a video describing your solutions.
+6 (10%) For questions 1‐5, create a video describing your solutions. -- DONE
 '''
 
 
@@ -21,6 +21,30 @@ size = 0
 def nothing(x):
     pass
 
+def closest_image_par (img_list, base_cut, mask):
+    ideal = None
+    overall_comp = None
+    for i in range(len(img_list)):
+        img_list[i] = cv2.bitwise_and(img_list[i], img_list[i], mask=mask)
+        base_cut = cv2.bitwise_and(base_cut, base_cut, mask=mask)
+        img_list[i] = img_list[i].astype('int32')
+        base_cut = base_cut.astype('int32')
+        diff = np.subtract(img_list[i], base_cut)
+        diff = np.absolute(diff)
+        img_list[i] = img_list[i].astype('uint8')
+        (B, G, R) = cv2.split(diff)
+        B = np.sum(np.square(B))
+        G = np.sum(np.square(G))
+        R = np.sum(np.square(R))
+        overall = B + G + R
+        if not isinstance(overall_comp, np.intc):
+            overall_comp = overall
+            ideal = img_list[i]
+        else:
+            if overall_comp > overall:
+                overall_comp = overall
+                ideal = img_list[i]
+    return ideal
 
 def closest_image(img_list, base_cut):
     ideal = None
@@ -78,7 +102,7 @@ def get_par_kernel(size):
 
 
 def run():
-    cv2.namedWindow("image")
+    cv2.namedWindow("image", cv2.WINDOW_NORMAL)
     cv2.createTrackbar('Patch size', 'image', 5, 50, nothing)
     cv2.createTrackbar('Shape', 'image', 0, 1, nothing)
     cv2.createTrackbar('Start', 'image', 0, 1, nothing)
@@ -95,26 +119,60 @@ def run():
         start = cv2.getTrackbarPos('Start', 'image')
         if start and not finished:
             img_list = get_images("dataset")
-            for x in range(base_img.shape[0] // size):
-                print(x, "out of", base_img.shape[0] // size)
-                if finished:
-                    break
-                for y in range(base_img.shape[1] // size):
-                    start = cv2.getTrackbarPos('Start', 'image')
-                    if not start:
-                        finished = 1
-                        edited = base_img.copy()
+            if shape == 0: # square
+                for x in range(base_img.shape[0] // size):
+                    print(x, "out of", base_img.shape[0] // size)
                     if finished:
                         break
-                    base_cut = base_img[x * size: (x + 1) * size,
-                               y * size: (y + 1) * size]
-                    ideal = closest_image(img_list, base_cut)
-                    edited[x * size: (x + 1) * size,
-                    y * size: (y + 1) * size] = ideal
-                    cv2.imshow("image", edited)
-                    if cv2.waitKey(1) == "q":
+                    for y in range(base_img.shape[1] // size):
+                        start = cv2.getTrackbarPos('Start', 'image')
+                        if not start:
+                            finished = 1
+                            edited = base_img.copy()
+                        if finished:
+                            break
+                        base_cut = base_img[x * size: (x + 1) * size,
+                                   y * size: (y + 1) * size]
+                        ideal = closest_image(img_list, base_cut)
+                        edited[x * size: (x + 1) * size,
+                        y * size: (y + 1) * size] = ideal
+                        cv2.imshow("image", edited)
+                        if cv2.waitKey(1) == "q":
+                            break
+            else: # parallelogramm
+                mask = get_par_kernel(size)
+                edited = np.zeros(base_img.shape, dtype=np.uint8)
+                #cv2.imshow("mask", mask)
+                for x in range(base_img.shape[0] // size):
+                    print(x, "out of", base_img.shape[0] // size)
+                    if finished:
                         break
-                    #print(x, y)
+                    for y in range(2 * base_img.shape[1] // size):
+                        start = cv2.getTrackbarPos('Start', 'image')
+                        if not start:
+                            finished = 1
+                        if finished:
+                            break
+                        try:
+                            if (y == 0):
+                                base_cut = base_img[(x * size): ((x + 1) * size),
+                                           y * size: (y + 1) * size]
+                                ideal = closest_image_par(img_list, base_cut, mask)
+                                edited[x * size: (x + 1) * size,
+                                y * size: (y + 1) * size] = ideal
+                            else:
+                                base_cut = base_img[(x * size): ((x + 1) * size),
+                                           y * (size//2): (y + 2) * (size//2)]
+                                ideal = closest_image_par(img_list, base_cut, mask)
+                                edited[x * size: (x + 1) * size,
+                                y * (size//2): (y + 2) * (size//2)] = cv2.add(edited[x * size: (x + 1) * size,
+                                y * (size//2): (y + 2) * (size//2)], ideal)
+
+                            cv2.imshow("image", edited)
+                            if cv2.waitKey(1) == "q":
+                                break
+                        except:
+                            print("smth wrong")
             cv2.imwrite("edited.jpg", edited)
             dst = cv2.addWeighted(base_img, 0.3, edited, 0.7, 0)
             cv2.imshow('image', dst)
