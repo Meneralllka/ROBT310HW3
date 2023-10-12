d4d328adf885cafe29062ce6865a3b86b5482a28
Aroaky <alikhan.habiev@gmail.com>
2023-10-12 20:17:26 +0600

    added vasya to the project
    (cherry picked from commit 44e71efe0ccde3552a2caff9145e1b7289546ffe)

---
+++ dataset.py
@@ -0,0 +1,19 @@
+import cv2
+
+cap = cv2.VideoCapture("dataset.mp4")
+cnt = 0
+print("vasya")
+while 1:
+    ret, frame = cap.read()
+    if ret:
+        if cnt % 100 == 0:
+            #cv2.imshow("window", frame)
+            cv2.imwrite("dataset/" + str(cnt) + ".jpg", frame)
+            print(cnt, "saved")
+            if cv2.waitKey(1) == "q":
+                break
+        cnt += 1
+    else:
+        break
+cv2.destroyAllWindows()
+cap.release()
